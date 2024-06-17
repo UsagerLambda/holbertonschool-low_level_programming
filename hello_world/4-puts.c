561d79e80d4495a78984302a8c02eef62efe2c4b
UsagerLambda <clement.chassem@gmail.com>
2024-06-17 09:19:00 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main(void)/* A C program starts from the main () function */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+ 
 return (0);
 }
