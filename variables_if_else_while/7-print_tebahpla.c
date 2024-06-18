1f06045ac68671a79d63c9e196cc85ad44b66ee9
UsagerLambda <clement.chassem@gmail.com>
2024-06-18 10:38:04 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print the lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char  za;
+for (char za = 'z'; za >= 'a'; za--)
+{
+putchar(za);
+}
+{
+putchar('\n');
+}
+return (0);
+}
