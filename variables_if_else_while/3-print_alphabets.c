4ef08cb2a48b66ee3a45e51b68cad5eb3d4438e7
UsagerLambda <clement.chassem@gmail.com>
2024-06-18 08:59:25 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description : printf alphabet in lower case using only putchar 
- * Return: Alway 0 (Success) 
+ * Description : printf alphabet in lower case using only putchar
+ * Return: Alway 0 (Success)
  */
 int main(void)
 {
