33e24289ecc5402936a343324e38677297aeaaab
UsagerLambda <clement.chassem@gmail.com>
2024-06-18 10:20:20 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print all single digit number with no char
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = '1'; n <= '9'; n++)
+{
+putchar(n);
+}
+{
+putchar('\n');
+}
+return (0);
+}
