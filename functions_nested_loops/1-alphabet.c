92ebb95105c442142829c845e6ab2d5c107a5bd7
UsagerLambda <clement.chassem@gmail.com>
2024-06-19 11:36:46 +0000

    1-alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
- * Description: :)
- * Return: Always 0 (Success)
+ * print_alphabet - Entry point
+ * Description : printf alphabet in lower case using only putchar
+ * Return: Alway 0 (Success)
  */
 void print_alphabet(void)
 {
