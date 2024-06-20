77f22a9a635cb8d01bcadb89a1636ccbc06600db
UsagerLambda <clement.chassem@gmail.com>
2024-06-20 12:50:12 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,16 @@
 int print_last_digit(int n)
 {
 int a;
+if (n >= 0)
+{
 a = n % 10;
-_putchar(a);
+_putchar(a + '0');
+}
+else if (n < 0)
+{
+a = (n * -1) % 10;
+_putchar(a + '0');
+return (a);
+}
 return (0);
 }
