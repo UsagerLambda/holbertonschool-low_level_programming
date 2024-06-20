169968a981e205126787438dc577ab138f9ae80c
UsagerLambda <clement.chassem@gmail.com>
2024-06-20 15:46:54 +0000

    8-24

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,11 +1,32 @@
 #include "main.h"
 /**
- * print_last_digit  - Entry point
- * @n: variable int
- * Description : printf alphabet in lower case using only putchar
+ * jack_bauer  - Entry point
+ * void - void
+ * Description : print 00:00 to 23:59
  * Return: Alway 0 (Success)
  */
 void jack_bauer(void)
 {
-
+int hr1, hr2, mn1, mn2;
+for (hr1 = 0; hr1 <= 2; hr1++)
+{
+for (hr2 = 0; hr2 <= 9; hr2++)
+{
+if ((hr1 <= 1 && hr2 <= 9) || (hr1 <= 2 && hr2 <= 3))
+{
+for (mn1 = 0; mn1 <= 5; mn1++)
+{
+for (mn2 = 0; mn2 <= 9; mn2++)
+{
+_putchar(hr1 + '0');
+_putchar(hr2 + '0');
+_putchar(58);
+_putchar(mn1 + '0');
+_putchar(mn2 + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
 }
