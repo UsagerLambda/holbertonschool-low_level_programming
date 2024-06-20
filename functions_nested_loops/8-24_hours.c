6d6c8825a97a50a8cdddd73af62fbbac443cc0be
UsagerLambda <clement.chassem@gmail.com>
2024-06-20 13:19:11 +0000

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit  - Entry point
+ * @n: variable int
+ * Description : printf alphabet in lower case using only putchar
+ * Return: Alway 0 (Success)
+ */
+void jack_bauer(void)
+{
+
+}
