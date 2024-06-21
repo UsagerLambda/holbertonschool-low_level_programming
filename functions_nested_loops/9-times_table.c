0b4258a457e95c227980a5cd08a059a23ee1748d
UsagerLambda <clement.chassem@gmail.com>
2024-06-21 12:29:49 +0000

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,20 +7,34 @@
  */
 void times_table(void)
 {
-  int nm1, nm2, tab;
-    tab = 10;
-    for (nm2 = 0; nm2 < tab; nm2++)
-      {
-    for (nm1 = 0; nm1 < tab; nm1++)
-      {
-	nm1 = nm1 * nm2;
-	_putchar(nm1 + '0');
-	if (nm1 < tab)
-	  {
-	_putchar(',');
-	_putchar(' ');
-	  }
-      }
-    _putchar('\n');
-      }
+int lgn, col, tab, result, result1, result2;
+tab = 10;
+for (col = 0; col < tab; col++)
+{
+for (lgn = 0; lgn < tab; lgn++)
+{
+result = lgn * col;
+result2 = result % 10;
+result1 = result / 10;
+if (lgn < tab)
+{
+if (result1 > 0)
+{
+_putchar (result1 + '0');
+_putchar (result2 + '0');
+}
+else
+{
+_putchar (' ');
+_putchar (result2 + '0');
+}
+}
+if (lgn < 9)
+{
+_putchar (',');
+_putchar (' ');
+}
+}
+_putchar ('\n');
+}
 }
