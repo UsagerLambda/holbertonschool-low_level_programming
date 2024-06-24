b08f417de47c83d88173c5169108fc5316654cdc
UsagerLambda <clement.chassem@gmail.com>
2024-06-24 10:10:26 +0000

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * times_table  - Entry point
+ * void - void
+ * Description : print 9 times table, starting with 0
+ * Return: Alway 0 (Success)
+ */
+void print_line(int n)
