ad101b82918ee760cfd56b671509ee66a333c2b4
UsagerLambda <clement.chassem@gmail.com>
2024-06-24 12:08:57 +0000

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square  - Entry point
+ * @size: - void
+ * Description : print a square
+ * Return: Alway 0 (Success)
+ */
+void print_square(int size)
+{
+  int lgn, col;
+  if (size <= 0)
+    _putchar('\n');
+  else
+    for (lgn = 0; lgn < size; lgn++)
+      {
+	for (col = 0; col < size; col++)
+	  {
+	    _putchar('#');
+	  }
+	_putchar('\n');
+      }
+}
