1304fbfdb3f6f0e3acbe01687b79249d39bf8293
UsagerLambda <clement.chassem@gmail.com>
2024-06-24 12:50:29 +0000

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,13 @@ for (num = 1; num <= 100; num++)
 num3 = num % 3;
 num5 = num % 5;
 if (num3 == 0)
-printf("Fizz ");
+printf(" Fizz");
 else if (num5 == 0)
-printf("Buzz ");
+printf(" Buzz");
+else if (num != 1 && num != 100)
+printf(" %d", num);
 else
-printf("%d ", num);
+printf("%d", num);
 }
 return (0);
 }
