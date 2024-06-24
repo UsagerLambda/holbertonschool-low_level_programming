085af5b62fcb3ca9837e87aa46d057cd5273de17
UsagerLambda <clement.chassem@gmail.com>
2024-06-24 07:29:36 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper  - Entry point
+ * @c: variable int
+ * Description : printf alphabet in lower case using only putchar
+ * Return: Alway 0 (Success)
+ */
+int _isupper(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
