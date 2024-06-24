72fd266874e38c1bd11dd50201b82597ed9e85d8
UsagerLambda <clement.chassem@gmail.com>
2024-06-24 12:10:04 +0000

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,16 +7,16 @@
  */
 void print_square(int size)
 {
-  int lgn, col;
-  if (size <= 0)
-    _putchar('\n');
-  else
-    for (lgn = 0; lgn < size; lgn++)
-      {
-	for (col = 0; col < size; col++)
-	  {
-	    _putchar('#');
-	  }
-	_putchar('\n');
-      }
+int lgn, col;
+if (size <= 0)
+_putchar('\n');
+else
+for (lgn = 0; lgn < size; lgn++)
+{
+for (col = 0; col < size; col++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
