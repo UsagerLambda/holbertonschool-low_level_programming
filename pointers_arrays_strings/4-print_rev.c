e6e9f789dd453b3dfbf5af1b413e07f52bd32830
UsagerLambda <clement.chassem@gmail.com>
2024-06-25 11:50:45 +0000

    4-print_rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ while (s[i])
 {
 i++;
 }
-while (i + 1)
+while (i >= 0)
 {
 _putchar(s[i]);
 i--;
---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * @s: tableau
+ * Description : return lenght of *s
+ * Return: Alway 0 (Success)
+ */
+void rev_string(char *s)
+{
+
+}
