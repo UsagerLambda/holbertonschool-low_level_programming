59488ba17af5130c9ae241e57c0d61e309213c22
UsagerLambda <clement.chassem@gmail.com>
2024-06-25 11:05:34 +0000

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * @s: tableau
+ * Description : print string in reverse
+ * Return: Alway 0 (Success)
+ */
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+{
+i++;
+}
+while (i + 1)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
