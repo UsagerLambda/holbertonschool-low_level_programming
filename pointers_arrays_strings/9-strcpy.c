7611a070857d949437c14da7b2ea23757ba5f2f8
UsagerLambda <clement.chassem@gmail.com>
2024-06-26 12:49:14 +0000

    9-strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- * puts_half - Entry point
- * @str: tableau
- * Description : print string starting from half of tab
+ * _strcpy - Entry point
+ * @dest:
+ * @src:
+ * Description : copy the string from src to dest
  * Return: Alway 0 (Success)
  */
 char *_strcpy(char *dest, char *src)
 {
-
+char *p_dest = dest;
+while ((*dest++ = *src++));
+return p_dest;
 }
