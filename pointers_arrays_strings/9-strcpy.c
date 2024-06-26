c7c6a91b5af19041ab341e65fd475091e7410375
UsagerLambda <clement.chassem@gmail.com>
2024-06-26 12:51:52 +0000

    9-strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * _strcpy - Entry point
- * @dest:
- * @src:
+ * @src: source de string
+ * @dest: destination de la copy de src
  * Description : copy the string from src to dest
  * Return: Alway 0 (Success)
  */
 char *_strcpy(char *dest, char *src)
 {
 char *p_dest = dest;
-while ((*dest++ = *src++));
-return p_dest;
+while ((*dest++ = *src++))
+;
+return (p_dest);
 }
