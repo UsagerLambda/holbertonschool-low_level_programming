c8d0f4a8eb4d2e99f18b5a5c43b6b363dc9cde56
UsagerLambda <clement.chassem@gmail.com>
2024-06-26 19:22:52 +0000

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - Entry point
+ * @s: source de string
+ * Description : copy the string from src to dest
+ * Return: Alway 0 (Success)
+ */
+int _atoi(char *s)
+{
+  int count = 0;
+while (s[count])
+{
+  if (s[count] >= '0' && s[count] <= '9')
+    {
+      printf("%c", s[count]);
+    }
+    count++;
+}
+ return (0);
+}
