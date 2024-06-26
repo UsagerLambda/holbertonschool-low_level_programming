9c2ade84bf87d046ee19c389b6454c2caa8b6159
UsagerLambda <clement.chassem@gmail.com>
2024-06-26 12:23:23 +0000

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * puts_half - Entry point
+ * @str: tableau
+ * Description : print string starting from half of tab
+ * Return: Alway 0 (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+}
