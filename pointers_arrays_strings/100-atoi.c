89e547061a2c3d2c1a956d5bd8ff1a2ce54a7691
UsagerLambda <clement.chassem@gmail.com>
2024-06-27 10:16:04 +0000

    100-atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,21 +1,25 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * _atoi - Entry point
  * @s: source de string
- * Description : copy the string from src to dest
+ * Description : convert string to an integer
  * Return: Alway 0 (Success)
  */
 int _atoi(char *s)
 {
-  int i = 0, num = 0, sign = 1;
-  while (s[i])
-    {
-      if (s[i] == '-')
-	sign *= -1;
-      else if (s[i] >= '0' && s[i] <= '9')
-	num = num * 10 + (s[i] - '0');
-      i++;
-    }
-  return (num * sign);
+int i = 0, num = 0, sign = 1, j = 0;
+while (s[i])
+{
+if (s[i] == '-' && !j)
+sign *= -1;
+else if (s[i] >= '0' && s[i] <= '9')
+{
+num = num * 10 + (s[i] - '0');
+j = 1;
+}
+else if (j)
+break;
+i++;
+}
+return (num * sign);
 }
