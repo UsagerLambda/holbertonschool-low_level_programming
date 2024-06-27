320c87a79d3f87ede512cadc1662edc423ddd9a0
UsagerLambda <clement.chassem@gmail.com>
2024-06-27 11:56:41 +0000

    0-strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * puts_half - Entry point
- * @str: tableau
- * Description : print string starting from half of tab
+ * _strcat - Entry point
+ * @dest: tableau destination
+ * @src: tableau source
+ * Description : do stuff
  * Return: Alway 0 (Success)
  */
 char *_strcat(char *dest, char *src)
 {
-  int a = 0;
-  char *p_dest = dest;
-  while (src[a])
-    a++;
-  dest[a] = src[a]
-    ;
-  return (p_dest);
+int a = 0;
+char *p_dest = dest;
+while (src[a])
+a++;
+dest[a] = src[a]
+;
+return (p_dest);
 }
