595a089201d694de74d845d2625b869fff71e901
UsagerLambda <clement.chassem@gmail.com>
2024-06-27 09:38:23 +0000

    100-atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,14 +8,14 @@
  */
 int _atoi(char *s)
 {
-  int i = 0, num = 0;
+  int i = 0, num = 0, sign = 1;
   while (s[i])
     {
-      if (s[i] == '+' || s[i] == '-')
-	
+      if (s[i] == '-')
+	sign *= -1;
       else if (s[i] >= '0' && s[i] <= '9')
 	num = num * 10 + (s[i] - '0');
       i++;
     }
-  return (num);
+  return (num * sign);
 }
