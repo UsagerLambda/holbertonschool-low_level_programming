53a69d12202d13cf071b44767ec2a04cac2f6c70
UsagerLambda <clement.chassem@gmail.com>
2024-06-28 12:21:26 +0000

    3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,13 @@
 int _strcmp(char *s1, char *s2)
 {
 for (;*s1 != '\0' ; s1++, s2++)
-if (*s1 == *s2)
-{
-return (0);
-}
-else if (*s1 != *s2)
+if (*s1 != *s2)
 {
 return (*s1 - *s2);
+}
+else
+{
+return (0);
 }
 return (0);
 }
