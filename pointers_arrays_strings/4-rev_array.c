17bb4cba1645e80321749581d928a21fa77469ca
UsagerLambda <clement.chassem@gmail.com>
2024-06-28 13:12:12 +0000

    4-rev_array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,15 +9,15 @@
  */
 void reverse_array(int *a, int n)
 {
-  int start = 0;
-  int end = n - 1;
-  int temp;
-  while (start < end)
-    {
-      temp = a[start];
-      a[start] = a[end];
-      a[end] = temp;
-      start++;
-      end--;
-    }
+int start = 0;
+int end = n - 1;
+int temp;
+while (start < end)
+{
+temp = a[start];
+a[start] = a[end];
+a[end] = temp;
+start++;
+end--;
+}
 }
