8a1d790a4d7bff4cfeb4d135aecd37651532666a
UsagerLambda <clement.chassem@gmail.com>
2024-06-28 19:02:06 +0000

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *cap_string - Entry point
+ * @t: max number of character
+ * Description : do stuff
+ * Return: Alway 0 (Success)
+ */
+char *cap_string(char *t)
+{
+  int i = 0;
+  while (t[i])
+    {
+      if (t[i + 1] >= 'a' && t[i + 1] <= 'z')
+	t[i + 1] = t[i + 1] - 32;
+	}
+  
+    }
+  return (t);
+}
