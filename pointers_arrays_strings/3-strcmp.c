6b2ebcac9c81e13bf88af67b0be7a9348a712756
UsagerLambda <clement.chassem@gmail.com>
2024-06-28 11:31:33 +0000

    3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strncat - Entry point
- * @dest: tableau destination
- * @src: tableau source
- * @n: max number of character
- * Description : do stuff
+ * _strcmp - Entry point
+ * @s1: tableau destination
+ * @s2: tableau source
+ * Description : compare two strings
  * Return: Alway 0 (Success)
  */
 int _strcmp(char *s1, char *s2)
 {
-  int a = 0;
-  a = *s1 - *s2;
-  while (*s1 && (*s1 == *s2))
-    {
-      s1++;
-      s2++;
-    }
-  return (a);
+int a = 0;
+a = *s1 - *s2;
+while (*s1 && (*s1 == *s2))
+{
+s1++;
+s2++;
+}
+return (a);
 }
