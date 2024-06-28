c85b4d88a011c7bfece86319b4445b8f668d4294
UsagerLambda <clement.chassem@gmail.com>
2024-06-28 12:16:22 +0000

    3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int a = 0;
-a = *s1 - *s2;
-while (*s1 != '\0' && (*s1 == *s2) && *s2 != '\0')
+for (;*s1 != '\0' ; s1++, s2++)
+if (*s1 == *s2)
 {
-s1++;
-s2++;
+return (0);
 }
-return (a);
+else if (*s1 != *s2)
+{
+return (*s1 - *s2);
+}
+return (0);
 }
