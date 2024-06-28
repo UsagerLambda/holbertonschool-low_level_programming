2a916ce59d8ee32832cce944b6f9ecd6e7992f2d
UsagerLambda <clement.chassem@gmail.com>
2024-06-28 14:55:39 +0000

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - Entry point
+ * @dest: tableau destination
+ * @src: tableau source
+ * @n: max number of character
+ * Description : do stuff
+ * Return: Alway 0 (Success)
+ */
+char *string_toupper(char *)
+{
+  int i = 0;
+  while (*[i])
+    for (; *[i] > *['A'] && *[i] < *['Z']; i++)
+      {
+      }
+      return (0);
+}
