6b3f63db45cb4f1a1879762299942daea16da6de
UsagerLambda <clement.chassem@gmail.com>
2024-06-29 08:43:26 +0000

    7-1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,8 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *cap_string - Entry point
+ * *leet - Entry point
  * @t: max number of character
- * Description : do stuff
+ * Description : encodes string into 1337 (leet)
  * Return: Alway 0 (Success)
  */
+char *leet(char *t)
+{
+int i = 0;
+char leet[256];
+leet['a'] = leet['A'] = '4';
+leet['e'] = leet['E'] = '3';
+leet['o'] = leet['O'] = '0';
+leet['t'] = leet['T'] = '7';
+leet['l'] = leet['L'] = '1';
+while (t[i])
+{
+if (leet[(unsigned char)t[i]] != 0)
+{
+t[i] = leet[(unsigned char)t[i]];
+}
+i++;
+}
+return (t);
+}
