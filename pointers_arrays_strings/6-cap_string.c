bcbd3b3901db8d9965bbea7f8ecb1a56f9eef75a
UsagerLambda <clement.chassem@gmail.com>
2024-06-29 06:59:42 +0000

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,13 @@
  */
 char *cap_string(char *t)
 {
-  int i = 0;
-  while (t[i])
-    {
-      if (t[i] == ' ')
-	if (t[i + 1] >= 'a' && t[i + 1] <= 'z')
-	  t[i + 1] = t[i + 1] - 32;
-      i++;
-    }
-  return (t);
+int i = 0;
+while (t[i])
+{
+if (t[i] == ' ')
+if (t[i + 1] >= 'a' && t[i + 1] <= 'z')
+t[i + 1] = t[i + 1] - 32;
+i++;
+}
+return (t);
 }
