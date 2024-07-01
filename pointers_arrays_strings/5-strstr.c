ca3403c98ad3aabbc0d7d0de086506e7e0add7f3
UsagerLambda <clement.chassem@gmail.com>
2024-07-01 17:23:41 +0000

    4-strpbrk

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strspn - Entry point
+ * @s: string
+ * @accept: string 2
+ * Description : get the length of the prefix substring
+ * Return: Alway 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}  
