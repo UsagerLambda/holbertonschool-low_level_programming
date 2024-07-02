127ee0080bcb395ed551667b16875b3119d54e69
UsagerLambda <clement.chassem@gmail.com>
2024-07-02 13:21:43 +0000

    0-puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,11 +1,17 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ * _puts_recursion - Entry point
+ * @s: string
+ * Description : print a string using recursion
  * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
-  *s * _puts_recursion(s - 1);
-  printf("%c", *s);
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
