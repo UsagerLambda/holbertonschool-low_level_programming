d95b6741ee55d63a354941d2a8e62724e8bc33d9
UsagerLambda <clement.chassem@gmail.com>
2024-07-04 13:23:21 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: args count
+ * @argv: unused
+ * Description : print the number of args
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
