85eb1ab77d5b8da5de64cb91044cce8d97d9acfc
UsagerLambda <clement.chassem@gmail.com>
2024-07-04 11:17:57 +0000

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,16 +1,31 @@
 /**
- * _puts_recursion - Entry point
- * @s: string
- * Description : print a string using recursion
+ * increment - Entry point
+ * @n: number
+ * @i: increment number
+ * Description : check if n correspond to if
  * Return: Always 0.
  */
-int is_prime_number(int n)
+int increment(int n, int i)
 {
 if (n <= 1)
 return (0);
 if (n == 2)
 return (1);
-if (n % 2 == 0)
+if (i * i > n)
+return (1);
+if (n % i == 0)
 return (0);
-return (is_prime_number(n + 1));
+return (increment(n, i + 1));
+}
+/**
+ * is_prime_number - Entry point
+ * @n: number
+ * Description : check if n < 2 if not go to function increment
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+if (n < 2)
+return (0);
+return (increment(n, 2));
 }
