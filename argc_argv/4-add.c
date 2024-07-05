55ef9a5124d41f02c2232a2dfd060af011306c27
UsagerLambda <clement.chassem@gmail.com>
2024-07-05 13:11:11 +0000

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -4,12 +4,13 @@
  * main - Entry point
  * @argc: args count
  * @argv: arguments
- * Description : add argv[1] with argv[2], argv[3], argv[4], argv[5]
+ * Description : add argv with the other argv
  * Return: Always 0.
  */
+
 int main(int argc, char *argv[])
 {
-int i = 0;
+int i = 0, j = 0;
 int result = 0;
 if (argc < 2)
 {
@@ -17,7 +18,23 @@ printf("0\n");
 return (1);
 }
 for (i = 1; i < argc; i++)
-result += atoi(argv[i]);
+{
+int valid = 1;
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+valid = 0;
+break;
+}
+}
+if (!valid)
+{
+printf("Error\n");
+return (1);
+}
+result = result + atoi(argv[i]);
+}
 printf("%d\n", result);
 return (0);
 }
