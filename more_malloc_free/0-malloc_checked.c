7862a2fbd3acd4c8fdd9eda857f40e30bf5ac300
UsagerLambda <clement.chassem@gmail.com>
2024-07-08 17:29:49 +0000

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 /**
- * create_array - Entry point
- * @size: size of the array
- * @c: content of array
+ * malloc_checked - Entry point
+ * @b: value
  * Description : create a array of chars
  * Return: Always 0.
  */
 void *malloc_checked(unsigned int b)
-{}
+{
+void *ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
+}
