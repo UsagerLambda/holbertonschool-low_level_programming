b1cbd65f56ad1599fd607e64983a6f24d242a434
UsagerLambda <clement.chassem@gmail.com>
2024-07-08 16:17:10 +0000

    more_malloc_free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+/**
+ * create_array - Entry point
+ * @size: size of the array
+ * @c: content of array
+ * Description : create a array of chars
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{}
