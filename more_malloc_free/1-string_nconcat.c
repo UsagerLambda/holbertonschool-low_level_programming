2d8265eb3f95cf328f3bbd5cfe56aaeea3835f18
UsagerLambda <clement.chassem@gmail.com>
2024-07-08 18:50:52 +0000

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ * string_nconcat - Entry point
+ * @s1: string1
+ * @s2: string2
+ * @n:
+ * Description : put s2 after s1
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
