a9bc6f746d81290bd70ad501cb6a513dbb3cbc1d
UsagerLambda <clement.chassem@gmail.com>
2024-07-09 08:58:11 +0000

    2-calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * _calloc - Entry point
+ * @nmemb: numbers of elements in array
+ * @size: size of bytes
+ * Description : alloc memory to a array
+ * Return: Always 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *array;
+if (nmemb == 0 || size == 0)
+return (NULL);
+array = malloc(sizeof(size) * nmemb);
+if (array == NULL)
+return (NULL);
+return (array);
+}
