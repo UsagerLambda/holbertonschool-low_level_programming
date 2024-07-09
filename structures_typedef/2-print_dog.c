a364558636deb13d70eaa7a471f2f8f4bd03383f
UsagerLambda <clement.chassem@gmail.com>
2024-07-09 15:11:04 +0000

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,9 +8,16 @@
  */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-printf("(nil)");
+if (d->name == NULL)
+printf("Name: (nil)");
+else
 printf("Name: %s\n", d->name);
+if (d->age < 0)
+printf("Age: (nil)");
+else
 printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+printf("Owner: (nil)");
+else
 printf("Owner %s\n", d->owner);
 }
