2592d471908f36fbd7f65ac9ce4a24fa35d43c6a
UsagerLambda <clement.chassem@gmail.com>
2024-07-09 16:02:54 +0000

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - Entry point
+ * @d: Pointer to the `dog` structure
+ * Description: print infos
+ */
+dog_t *new_dog(char *name, float age, char *owner)
