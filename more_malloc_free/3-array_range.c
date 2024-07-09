e65932d536d24e12eb76f7955a3cb56befe21279
UsagerLambda <clement.chassem@gmail.com>
2024-07-09 11:20:14 +0000

    3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * _calloc - Entry point
+ * @nmemb: numbers of elements in array
+ * @size: size of bytes
+ * Description : alloc memory to a array
+ * Return: Always 0.
+ */
+int *array_range(int min, int max)
+{
+    
+}
