9621444c7e592607c87a57246ce095c3ba5cd16a
UsagerLambda <clement.chassem@gmail.com>
2024-07-09 13:39:55 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Information about a dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ * Description: Information about a dog
+ */
 struct dog
 {
 char *name;
