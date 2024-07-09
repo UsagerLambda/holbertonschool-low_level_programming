c6c7906acb2881e327b685e72b28351f97157392
UsagerLambda <clement.chassem@gmail.com>
2024-07-09 12:27:15 +0000

    3-array_range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,12 +1,21 @@
 #include <stdlib.h>
 /**
- * _calloc - Entry point
- * @nmemb: numbers of elements in array
- * @size: size of bytes
- * Description : alloc memory to a array
+ * array_range - Entry point
+ * @min: minimal value
+ * @max: maximal value
+ * Description : create a array of integer from min to max
  * Return: Always 0.
  */
 int *array_range(int min, int max)
 {
-    
+int *array, r, i;
+r = max - min + 1;
+if (min > max)
+return (NULL);
+array = malloc(sizeof(int) * r);
+if (array == NULL)
+return (NULL);
+for (i = 0; i < r; i++)
+array[i] = min + i;
+return (array);
 }
