8c69ae6f2a5c87591222d8cc81cf5a522c347360
UsagerLambda <clement.chassem@gmail.com>
2024-07-09 08:33:05 +0000

    1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,27 +10,31 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *array;
-int i = 0, r = 0, o2 = 0, o = 0;
-unsigned int r2;
+unsigned int i = 0, o = 0, r = 0, o2 = 0;
 while (s1[i] != '\0')
 i++;
+while (s2[o] != '\0')
+o++;
+if (n >= o)
+r = i + o;
+else
 r = i + n;
 array = malloc(sizeof(char) * (r + 1));
 if (array == NULL)
 return (NULL);
 i = 0;
+o = 0;
 while (s1[i] != '\0')
 {
 array[i] = s1[i];
 i++;
 }
 o = i + o;
-while ((s2[o2] != '\0') && (r2 < n))
+while ((s2[o2] != '\0') && (o2 < n))
 {
 array[o] = s2[o2];
 o++;
 o2++;
-r2++;
 }
 array[o] = '\0';
 return (array);
