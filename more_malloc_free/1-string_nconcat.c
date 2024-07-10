784c60db55c1f3019f8df9e76b21b8c689bb301a
UsagerLambda <clement.chassem@gmail.com>
2024-07-10 08:26:48 +0000

    1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *array;
 unsigned int i = 0, o = 0, r = 0, o2 = 0;
 if (s1 == NULL)
-s1 == ""; 
+s1 == (""); 
 if (s2 == NULL)
-s2 == "";
+s2 == ("");
 while (s1[i] != '\0')
 i++;
 while (s2[o] != '\0')
