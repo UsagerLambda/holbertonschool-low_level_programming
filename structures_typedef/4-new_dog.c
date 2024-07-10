cc0c9f1d3c2044663e08a8012020f2326b525d4c
UsagerLambda <clement.chassem@gmail.com>
2024-07-10 14:28:42 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,33 +3,33 @@
 #include "dog.h"
 /**
  * new_dog - Entry point
- * @d: Pointer to the `dog` structure
- * @name:
- * @age:
- * @owner:
+ * @name: name of the cloned dog
+ * @age: age of the cloned dog
+ * @owner: owner of the cloned dog
+ * Return: return
  * Description: print infos
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    if (name == NULL || age == 0 || owner == NULL)
-    return (NULL);
-    dog_t *dog;
+	if (name == NULL || age == 0 || owner == NULL)
+	return (NULL);
+	dog_t *dog;
 
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-    return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	return (NULL);
 	dog->age = age;
-    dog->name = _strdup(name);
-    if (dog->name == NULL)
-    {
-    free(dog);
-    return (NULL);
+	dog->name = _strdup(name);
+	if (dog->name == NULL)
+	{
+	free(dog);
+	return (NULL);
 }
-    dog->owner = _strdup(owner);
-    {
-      free(dog->name);
-    free(dog);
-    return (NULL);
+	dog->owner = _strdup(owner);
+{
+	free(dog->name);
+	free(dog);
+	return (NULL);
 }
-    return (dog);
+	return (dog);
 }
