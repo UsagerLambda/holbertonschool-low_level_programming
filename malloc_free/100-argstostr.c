cbbeb17bf8d1e42052540194f934fcddefd08000
UsagerLambda <clement.chassem@gmail.com>
2024-07-12 09:39:42 +0000

    100-argstostr.c

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * argstostr - Entry point
@@ -10,12 +11,27 @@
  */
 char *argstostr(int ac, char **av)
 {
-int i = 0;
-if (ac == 0 || av == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
+	int i = 0, l = 0, j, k = 0;
+	char *r;
+
+	if (ac == 0 || av == NULL)
+	return (NULL);
+	for (i = 0; i < ac; i++)
 {
-printf("%s\n", av[i]);
+	for (j = 0; av[i][j]; j++)
+	l++;
+	l++;
 }
-return (0);
+	l++;
+	r = (char *)malloc(sizeof(char) * l);
+	if (r == NULL)
+	return (NULL);
+	for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++)
+	r[k++] = av[i][j];
+	r[k++] = '\n';
+}
+	r[k] = '\0';
+	return (r);
 }
