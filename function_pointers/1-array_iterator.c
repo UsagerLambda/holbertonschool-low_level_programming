cb981c9d5408875b01c3c90330d5c1b709887b5c
UsagerLambda <clement.chassem@gmail.com>
2024-07-15 09:52:37 +0000

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f: pointeur vers une fonction
+ * array_iterator - itère dans le tableau et applique la fonction action
+ * @array: pointeur vers un tableau d'entier
+ * @size: taille du tableau
+ * @action: pointeur vers une fonction qui prend un entier en argument
  * Return: pas de return prototype void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
@@ -10,10 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 /** en argument et ne retourne rien (void) */
 {
 unsigned int i;
+
 if (array && size > 0 && action)
 /** Vérifie que array, size et la fonction action ne sont pas nuls */
 {
-    for (i = 0; i < size; i++) /** parcours le tableau*/
-    action(array[i]);
+	for (i = 0; i < size; i++) /** parcours le tableau*/
+	action(array[i]); /** action devient soit print_elem soit print_elem_hex */
 }
 }
