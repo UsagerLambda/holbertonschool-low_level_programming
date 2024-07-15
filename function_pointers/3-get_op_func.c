42e9d88c9a9fc889ea329f29d8640c10e45e5625
UsagerLambda <clement.chassem@gmail.com>
2024-07-15 15:56:44 +0000

    3-main.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
+/** prend un argument char *s (string of chars)*/
+/** retourne un pointeur vers deux entiers*/
 int (*get_op_func(char *s))(int, int);
+{
+/** Déclaration du tableau de struct contenant */
+/** les opérateurs et les fonctions associées */
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+int i;
+}
