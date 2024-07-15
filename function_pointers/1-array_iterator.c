3d070dbb03bf285ed3fa829ab9f8649634d46ef2
UsagerLambda <clement.chassem@gmail.com>
2024-07-15 08:37:17 +0000

    0-print_name.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: pointeur vers une fonction
+ * Return: pas de return prototype void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (array && size)
+size(array);
+}
