0de5383cdbf30aec0631fbdddc1a747738ec8c48
UsagerLambda <clement.chassem@gmail.com>
2024-07-15 08:01:08 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - prints a name as is
  * @name: name of the person
@@ -5,3 +6,13 @@
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
+{
+	if (name && f) /** si name et f ne sont pas NULL*/
+	f(name); /** f devient print_name_as_is ou print_name_uppercasse */
+	/** et name de vient Bob ou Bob Dylan*/
+	/** dans main ont appelle print_name en donnant name = Bob */
+	/** et en demandant la fonction print_name_as_is */
+	/** ou Bob Dylan avec print_name_uppercase*/
+	/** f(name) devient soit print_name_as_is(Bob) */
+	/** ou print_name_uppercase(Bob Dylan) */
+}
