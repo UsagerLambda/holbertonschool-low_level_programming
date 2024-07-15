41cb055f914aff88403209d59bc5da70ac0eed6a
UsagerLambda <clement.chassem@gmail.com>
2024-07-15 08:42:02 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -8,11 +8,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f) /** si name et f ne sont pas NULL*/
-	f(name); /** f devient print_name_as_is ou print_name_uppercasse */
-	/** et name de vient Bob ou Bob Dylan*/
-	/** dans main ont appelle print_name en donnant name = Bob */
-	/** et en demandant la fonction print_name_as_is */
-	/** ou Bob Dylan avec print_name_uppercase*/
-	/** f(name) devient soit print_name_as_is(Bob) */
-	/** ou print_name_uppercase(Bob Dylan) */
+	f(name); /** appelle la fonction pointer par f avec name comme argument */
 }
