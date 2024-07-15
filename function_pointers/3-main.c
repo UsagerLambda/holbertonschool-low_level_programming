ec32f116647ad3ab33d28a5298b9d980570d7ac4
UsagerLambda <clement.chassem@gmail.com>
2024-07-15 14:52:00 +0000

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,23 +1,16 @@
 #include <stdlib.h>
 #include "3-calc.h"
 /**
- * array_iterator 
- * @array: 
- * @size: 
- * @action: 
+ * main - Entry point
+ * @argc: nombre d'arguments
+ * @argv: argument
  * Return: 
  */
 int main(argc, *argv[])
 {
-
 if (argc != 4)
 {
 	printf("Error\n");
 	exit(98);
 }
-num1 = atoi(argv[1]);
-operator = argv[2];
-num2 = atoi(argv[3]);
-
-if (argv[i][0] != '+' && argv[i][0] != '-' && argv[i][0] != '*' && argv[i][0] != '/' && argv[i][0] != '%')
 }
