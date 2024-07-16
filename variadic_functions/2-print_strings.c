8bebe9787b5a545e07efa8bf2de06adef1a94a22
UsagerLambda <clement.chassem@gmail.com>
2024-07-16 14:52:06 +0000

    2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - print les entiers de la chaine variadic
+ * @separator: chaine de caractère ','
+ * @n: valeur du premiers nombre de la liste
+ * @...: varadic paramètre
+ * Return: Alway 0 (Success)
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i; /** variable pour parcourir n*/
+	va_list args; /** variable args qui va stocker la liste des arguments. */
+
+	va_start(args, n); /** initialise la liste d'arguments args */
+	/** et place le pointeur sur le premier argument après n */
+	for (i = 0; i < n; i++) /** déplace i dans la chaine */
+{
+	printf("%s", va_arg(args, char)); /** print le caractère pointer par i */
+	if (separator != NULL && i < n - 1) /** si séparator n'est pas NULL */
+	/** et i est inférieur à 3 */
+	printf("%s", separator); /** si c'est le cas print separator (',') */
+}
+	va_end(args); /** termine la gestion de la liste d'arguments. */
+	printf("\n"); /** saute une ligne à la fin */
+}
