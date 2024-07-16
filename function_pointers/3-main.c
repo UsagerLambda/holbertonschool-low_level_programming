b49e09119341dcd5620df4ec6cf678a6fa3bc585
UsagerLambda <clement.chassem@gmail.com>
2024-07-16 09:34:57 +0000

    3-betty

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include "3-calc.h"
 /**
- * main - Entry point
+ * main - programme principal pour calculer deux nombres
  * @argc: nombre d'arguments
  * @argv: tableau de chaine de caractère représenant les arguments
  * Return: 0 si success, sinon une autre valeur en cas d'erreur
@@ -40,23 +40,22 @@ printf("%d\n", operator(a, b));
 return (0);
 }
 
-/**
- * En prenant pour input 3 + 8 :
- * argv[0] = ./calc       | argv[1] = 3   | argv[2] = + | argv[3] = 8
- * Ligne 17 vérifie si le nombre d'arguments est respecté. Dans ce cas, oui.
- * Ensuite, convertit argv[1] & argv[3] en entiers puis les assigne à a & b.
- * À la ligne 26, fonction get_op_func est appelée avec l'opérateur argv[2].
- * Tableau ops est parcouru pour trouver fonction associée à l'opérateur '+'
- * (car argv[2] est '+'). On compare argv[2] avec chaque élément du tableau
- * jusqu'à trouver le même opérateur.
- * Pour '+', on trouve op_add. On assigne op_add à la variable operator.
- * Si operator = NULL (donc introuvable), affiche "Error" puis exit avec 99.
- * Ce n'est pas le cas (car operator = op_add), donc on continue.
- * À la ligne 34, on vérifie si operator est '/' ou '%' et si b = 0.
- * Si c'est le cas, on affiche "Error" puis on sort avec le code 100.
- * À la ligne 39, on affiche le résultat de la fonction operator = op_add,
- * donc op_add ayant a et b comme arguments (a = 3 & b = 8).
- * La fonction op_add renvoie le résultat de a + b.
- * Donc on affiche le résultat de a + b = 11.
- * Résultat final : 11
- */
+
+ /** En prenant pour input 3 + 8 */
+ /** argv[0] = ./calc       | argv[1] = 3   | argv[2] = + | argv[3] = 8 */
+ /** Ligne 17 vérifie si le nombre d'arguments respecté. Dans ce cas, oui. */
+ /** Puis, convert argv[1] & argv[3] en entiers puis les assigne à a & b. */
+ /** Ligne 26, fonction get_op_func est appelée avec l'opérateur argv[2]. */
+ /** Array ops parcouru pour trouver fonction associée à l'opérateur '+' */
+ /** (car argv[2] est '+'). Compare argv[2] avec chaque élément du tableau */
+ /** jusqu'à trouver le même opérateur. */
+ /** Pour '+', on trouve op_add. On assigne op_add à la variable operator. */
+ /** Si operator = NULL (donc introuvable), affiche "Error" puis exit 99. */
+ /** Ce n'est pas le cas (car operator = op_add), donc on continue. */
+ /** À la ligne 34, on vérifie si operator est '/' ou '%' et si b = 0. */
+ /** Si c'est le cas, on affiche "Error" puis on sort avec le code 100. */
+ /** Ligne 39, on affiche le résultat de la fonction operator = op_add, */
+ /** donc op_add ayant a et b comme arguments (a = 3 & b = 8). */
+ /** La fonction op_add renvoie le résultat de a + b. */
+ /** Donc on affiche le résultat de a + b = 11. */
+ /** Résultat final : 11 */
