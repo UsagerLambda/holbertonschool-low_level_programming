70c6b70337b9ca2a76505123dabfb805dd1923b0
UsagerLambda <clement.chassem@gmail.com>
2024-07-17 12:01:22 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,16 +1,42 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * print_strings - imprime des chaînes de caractères
- * @separator: chaine de caractère ','
- * @n: valeur du premiers nombre de la liste
+ * print_all - imprime des chaînes de caractères
+ * en fonction du format de données
+ * @format: liste des types de données
  * @...: varadic paramètre
  */
 void print_all(const char * const format, ...)
 {
-va_list args;
-va_start(args, format);
-printf("%s", format);
-va_end(args);
-printf("\n");
+	va_list args; /** création de ma liste */
+	const char *str; /** pointeur vers va_arg(args char*) */
+	char *separator = ", "; /** chaine ", " */
+	int i = 0; /** i pour parcourir format */
+
+	va_start(args, format); /** début de ma liste */
+	while (format && format[i]) /** tant que format && format[i] != '\0' */
+{
+	/** si format[i] est égal à c, i ,f, s*/
+	switch (format[i])
+{
+	case 'c': /** si char*/
+	printf("%s%c", separator, va_arg(args, int));
+	break;
+	case 'i': /** si entier */
+	printf("%s%d", separator, va_arg(args, int));
+	break;
+	case 'f': /** si float */
+	printf("%s%f", separator, va_arg(args, double));
+	break;
+	case 's': /** si chaine de caractère */
+	str = va_arg(args, char*);
+	if (str == NULL)
+	str = "(nil)";
+	printf("%s%s", separator, str);
+	break;
+}
+	i++;
+}
+	va_end(args);
+	printf("\n");
 }
