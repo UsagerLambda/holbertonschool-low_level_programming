60f3e054c9f893259bafec970d429eed0fc6213e
UsagerLambda <clement.chassem@gmail.com>
2024-07-17 17:39:27 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,43 +1,89 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
+
 /**
- * print_all - imprime des chaînes de caractères
- * en fonction du format de données
- * @format: liste des types de données
- * @...: varadic paramètre
+ * print_char - Imprime un caractère
+ * @args: Liste d'arguments contenant le caractère à imprimer
+ */
+void print_char(va_list args) /** va_list args : Liste arguments variables */
+{
+/** Extrait l'argument suivant de la liste */
+/** en le traitant comme un int (le type char est promu en int) */
+	printf("%c", va_arg(args, int)); /** printf print le caractère extrait */
+}
+/**
+ * print_int - Imprime un entier
+ * @args: Liste d'arguments contenant l'entier à imprimer
+ */
+void print_int(va_list args)
+{
+/** Extrait l'argument suivant comme un entier. */
+	printf("%d", va_arg(args, int)); /** printf print l'entier extrait */
+}
+/**
+ * print_float - Imprime un float
+ * @args: Liste d'arguments contenant le flot à imprimer
+ */
+void print_float(va_list args)
+{
+/** Extrait l'argument suivant comme un double. */
+/** (pour les float, double est utilisé dans va_arg) */
+	printf("%f", va_arg(args, double)); /** printf print le float extrait */
+}
+/**
+ * print_string - Imprime une chaîne de caractères
+ * @args: Liste d'arguments contenant la chaîne à imprimer
+ */
+void print_string(va_list args)
+{
+/** va_arg(args, char *) : Extrait l'arg suivant comme une string */
+/** et l'assigne à un pointeur de chaine de caractère "str" */
+	char *str = va_arg(args, char *);
+
+	if (str == NULL) /** si str = NULL */
+		str = "(nil)"; /** si oui remplace NULL par (nil) */
+	printf("%s", str); /** Imprime la chaîne de caractères */
+}
+
+/**
+ * print_all - Imprime divers types selon le format donné
+ * @format: Liste des types d'arguments passés à la fonction
  */
 void print_all(const char * const format, ...)
 {
-	va_list args; /** création de ma liste */
-	const char *str; /** pointeur vers va_arg(args char*) */
-	char *separator = ""; /** chaine ", " */
-	int i = 0; /** i pour parcourir format */
+/** tableau de structures associant les symbole à une fonction associée */
+	printer_t printers[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 
-	va_start(args, format); /** début de ma liste */
-	while (format && format[i]) /** tant que format && format[i] != '\0' */
+	va_list args; /** Déclare une liste d'arguments variables. */
+	int i = 0, j; /** Variables d'intération pour les boucles */
+	char *sep = ""; /** Initialise une chaîne vide pour gérer les sep. */
+/** Initialise args pour commencer à lire les arguments après format --> */
+	va_start(args, format);
+
+	while (format && format[i]) /** 1er boucle pour parcourir le format. */
 {
-	/** test l'égalité de case dans dans format[i] */
-	switch (format[i])
+	j = 0; /** set j à 0 */
+	/** 2eme boucle pour trouver le bon symbole */
+	while (printers[j].symbol != NULL)
 {
-	case 'c': /** si char*/
-	printf("%s%c", separator, va_arg(args, int));
-	break;
-	case 'i': /** si entier */
-	printf("%s%d", separator, va_arg(args, int));
-	break;
-	case 'f': /** si float */
-	printf("%s%f", separator, va_arg(args, double));
-	break;
-	case 's': /** si chaine de caractère */
-	str = va_arg(args, char*);
-	if (str == NULL)
-	str = "(nil)";
-	printf("%s%s", separator, str);
+	if (format[i] == *(printers[j].symbol))
+{
+	printf("%s", sep); /** Imprime le séparateur */
+	printers[j].print(args); /** call la fonction d'impression correspondante. */
+	sep = ", "; /** set sep à ", " après l'impression du 1er symbole */
 	break;
 }
-	separator = ", ";
-	i++;
+	j++; /** avance dans les symboles */
+	}
+	i++; /** avance dans format */
 }
-	va_end(args);
-	printf("\n");
+	va_end(args); /** Termine l'utilisation de la liste d'arguments variables. */
+	printf("\n"); /** saute une ligne à la fin */
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+/**
+ * struct printer - Structure associant un symbole à une fonction qui print
+ * @symbol: Chaîne de caractères contenant un symbole pour le type de donnée
+ * @print: Pointeur vers fonction prenant `va_list` et print le type de donnée
+ *
+ * Cette structure est utilisée pour associer chaque symbole
+ * à une fonction qui print ce type de donnée. Le tableau de ces
+ * structures est parcouru dans la fonction print_all pour call la fonction
+ * appropriée selon le symbole rencontré dans la chaîne de format.
+ */
+typedef struct printer
+{
+/** pointeur vers une chaîne de caractères qui représente un symbole */
+	char *symbol;
+/** pointeur vers une fonction qui prend une va_list comme argument */
+	void (*print)(va_list args);
+} printer_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
