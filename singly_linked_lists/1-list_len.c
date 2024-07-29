be29552d873991ac5192de9ce8e66898edec6ff5
UsagerLambda <clement.chassem@gmail.com>
2024-07-29 10:53:18 +0200

    P0T1 first itération

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 /**
- * list_len - Entry point
- * @h: standby
- * Return: NULL
+ * list_len - Return the number of elements in list_t list.
+ * @h: Pointer to the list_t list.
+ * Return: The number of nodes in the list.
  */
 size_t list_len(const list_t *h)
 {
-
+int i = 0;
+while (h != NULL)
+{
+h = h->next;
+i++;
+}
+return (i);
 }
