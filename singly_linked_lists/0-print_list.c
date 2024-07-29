2b4de150d800d899ffd528b93578d0646f95d842
UsagerLambda <clement.chassem@gmail.com>
2024-07-29 10:41:38 +0200

    fix betty P0TO, add files of POT1

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_list - Entry point
- * @h: Stanby
- * Return: Standby
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the list_t list to print.
+ * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
 {
---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * list_len - Entry point
+ * @h: standby
+ * Return: NULL
+ */
+size_t list_len(const list_t *h)
+{
+
+}
