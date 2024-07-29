68a4e4a987616a5837e92384832b05e595717bc6
UsagerLambda <clement.chassem@gmail.com>
2024-07-29 10:32:04 +0200

    verif str == NULL & print h->len

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,7 +10,10 @@ size_t print_list(const list_t *h)
 int i = 0;
 while (h != NULL)
 {
-printf("[%d] %s\n", i, h->str);
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
 h = h->next;
 i++;
 }
