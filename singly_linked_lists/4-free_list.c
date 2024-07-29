5e08b79c1f054c48cfa70354c8534dfce45e716e
UsagerLambda <clement.chassem@gmail.com>
2024-07-29 21:41:49 +0200

    P0T4 1er itération

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,9 +1,16 @@
 #include "lists.h"
 /**
- * free_list - Entry point
- * @head: NULL
+ * free_list - Free list_t
+ * @head: pointer to the head of list_t
  */
 void free_list(list_t *head)
 {
-
+list_t *temp;
+while (head != NULL)
+{
+temp = head; /* affecte head à temp */
+head = head->next; /* déplace head au node suivant */
+free(temp->str); /* libère la mémoire du node temp->str */
+free(temp); /* libère le node */
+}
 }
