e408c358eba5477e8deab215334b3eae34df3ac8
UsagerLambda <clement.chassem@gmail.com>
2024-07-29 10:57:53 +0200

    Add P0T2 files

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * add_node - Add new nodes at the beginning of the list_t list.
+ * @head: NULL
+ * @str: NULL
+ * Return: NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+}
