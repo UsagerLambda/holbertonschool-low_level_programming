bcd5267146899d884e70c28aa48033f738967f75
UsagerLambda <clement.chassem@gmail.com>
2024-07-29 15:07:55 +0200

    add P0T3 files

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * add_node_end - add new node at the end of list_t list.
+ * @head: NULL
+ * @str: NULL
+ * Return: NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+}
