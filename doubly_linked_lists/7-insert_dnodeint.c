428660bb008ce39966aaf22b91f63098491d53d1
UsagerLambda <clement.chassem@gmail.com>
2024-07-30 19:16:16 +0200

    add P1T7 files

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - Entry point
+ * @h: NULL
+ * @idx: NULL
+ * @n: NULL
+ * Return: NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+}
