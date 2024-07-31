069a2d7c8f0d872364b08d943433ef624ea19dfc
UsagerLambda <clement.chassem@gmail.com>
2024-07-31 14:36:49 +0200

    Fix Betty P1T7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,39 +9,30 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 unsigned int index = 0;
-dlistint_t *new_node, *temp;
+dlistint_t *new_node = n, *temp; /* n = 4096 */
+if (h == NULL)
+return (NULL);
 
-if (h == NULL) /* vérifie si le head est NULL */
-return (NULL); /* si oui return NULL */
-
-if (idx == 0) /* si idx est égal à 0 */
+if (idx == 0)
 return (add_dnodeint(h, n)); /* insère un nouveau node */
 
 temp = *h; /* assigne l'adresse de h à temp */
-/* vérifie si temp != NULL et index n'est pas égal à 4 (idx(5) - 1) */
-for (index = 0; temp != NULL && index != idx - 1; index++)
+for (index = 0; temp != NULL && index != idx - 1; index++) /* idx = 4 */
 temp = temp->next; /* passe au prochain node */
 
-if (temp == NULL) /* si temp est NULL */
-return (NULL); /* retourne NULL*/
-
-/* si temp->next est NULL et que index est égal à idx - 1 (4) */
-if (temp->next == NULL && index == idx - 1)
+if (temp == NULL)
+return (NULL);
+if (temp->next == NULL && index == idx - 1) /* idx = 4 */
 return (add_dnodeint_end(h, n)); /* add un node avec les args h et n */
 
-/* assigne à new_node la mémoire équivalente à dlistint_t */
 new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL) /* vérifie si l'alloc à échouée */
-return (NULL); /* si oui retourne NULL */
+if (new_node == NULL)
+return (NULL);
 
-new_node->n = n; /* assigne à new_node->n la valeur de n (4096) */
 new_node->next = temp->next; /* pareil avec next */
 new_node->prev = temp; /* assigne l'adresse de temp à new_node->prev */
-
-/* vérifie si temp->next est le dernier node de la liste */
-if (temp->next == NULL)
-/* accède au champ prev du node suivant et le definie sur new_node */
-temp->next->prev = new_node;
+if (temp->next == NULL) /* vérif si temp->next est le last node de la liste */
+temp->next->prev = new_node; /* assign new_node au champ prev du next node */
 
 temp->next = new_node; /* définie temp->next avec new_node */
 return (new_node); /* retourne new_node */
