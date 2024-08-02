c81452bb3e53011e358a4ff53ffd7c1c22639bc8
UsagerLambda <clement.chassem@gmail.com>
2024-08-02 21:39:37 +0200

    fix open P3T2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -7,12 +7,14 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, bytes_written, i = 0;
+int fd, i = 0;
+ssize_t bytes_written;
+char *cpy;
 
 if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY, O_APPEND);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 
@@ -22,7 +24,8 @@ if (text_content == NULL)
 	return (1);
 }
 
-while (text_content[i] != '\0')
+cpy = text_content;
+while (cpy[i] != '\0')
 i++;
 
 bytes_written = write(fd, text_content, i);
---
+++ file_io/3-cp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: --
+ */
+int main(void)
+{
+int fd, i;
+char *buffer;
+
+fd = open("incitatous", O_RDONLY);
+if (fd == -1)
+return (-1);
+
+}
